cbe03f660224fd8827cda7bc920a3978fb4c503a
SuperBoySugar <shveta23@gmail.com>
2023-03-16 19:50:13 +0000

    Jave Midweek 6 homework

---
+++ src/Programme_4_RightTriangle.java
@@ -0,0 +1,22 @@
+
+    /**
+     * 4. Write a Java Program to print below triagle.
+     *
+     * *
+     * **
+     * ***
+     * ****
+     * *****
+     * ******
+     */
+    public class Programme_4_RightTriangle {
+        public static void main(String[] args) {
+            System.out.println("*");
+            System.out.println("**");
+            System.out.println("***");
+            System.out.println("****");
+            System.out.println("*****");
+            System.out.println("******");
+        }
+
+}
